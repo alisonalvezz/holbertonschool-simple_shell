5ac2da7c44b3757807d38e127346c2fa005d37a5
Epicder <epicder11@gmail.com>
2024-04-16 14:10:12 +0000

    fix read_line Function

--- la_shellula.c
+++ la_shellula.c
@@ -18,21 +18,7 @@ void display_prompt(void)
         printf("shellula ");
         fflush(stdout);
 }
-
-<<<<<<< HEAD
-#include "simple_shell.h"
-
-void execute_command(char *cmd)
-{
-       /** -obtener comando*/
-        /**construir ruta al ejecutable ("/bin/" + comando)*/
-        /**verificar si el ejecutable es accesible (con access()*/
-        /**ejecutar el comando en un proceso hijo con fork donde:*/
-        /**construye lo que se precise para exceve*/
-        /**si no es accesible, mostrar error con perror y terminar _exit*/
-        /**si es accesible, ejecutar con execve*/
-        /**esperar que el proceso hijo termine y joya*/
-=======
+  
 char *read_line(void)
 {
 char *cmd = NULL;
@@ -44,5 +30,5 @@ size_t BUFSIZE = 0;
      exit(EXIT_FAILURE);
    }
   return (cmd);
->>>>>>> 7d0892c4d0dfa696326673e8d59a1ab21b00e049
+
 }
