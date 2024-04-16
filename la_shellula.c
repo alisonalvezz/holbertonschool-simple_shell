0a5b8b18bda261c8b3d2101e847000cab1d0f442
Alison <8661@holbertonschool.com>
2024-04-16 14:03:51 +0000

    commit

--- la_shellula.c
+++ la_shellula.c
@@ -22,6 +22,20 @@ int main()
 
 void display_prompt(void)
 {
-        printf("la_shellula ");
+        printf("shellula ");
         fflush(stdout);
+}
+
+#include "simple_shell.h"
+
+void execute_command(char *cmd)
+{
+       /** -obtener comando*/
+        /**construir ruta al ejecutable ("/bin/" + comando)*/
+        /**verificar si el ejecutable es accesible (con access()*/
+        /**ejecutar el comando en un proceso hijo con fork donde:*/
+        /**construye lo que se precise para exceve*/
+        /**si no es accesible, mostrar error con perror y terminar _exit*/
+        /**si es accesible, ejecutar con execve*/
+        /**esperar que el proceso hijo termine y joya*/
 }
---
+++ shellula_test.c
@@ -0,0 +1,21 @@
+#include "simple_shell.h"
+
+int main()
+{
+        /**char *cmd;*/
+        size_t buffer_size = 50;
+        char *buffer;
+
+        while (1)
+        {
+                display_prompt();
+                /**tiene que ser dentro del bucle porq sino no se ejecuta siempre*/
+                getline(&buffer, &buffer_size, stdin);
+                /**leer comando con getline desde la stdin*/
+                /*ejecutar comando con execute_command()*/
+                /**repetir para esperar el siuiente comando del user*/
+                /**execute_command(cmd);*/
+        }
+
+        return (0);
+}
