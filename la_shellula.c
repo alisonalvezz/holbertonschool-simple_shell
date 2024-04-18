23448642f5e32f815139ad815f603fb849e6b073
Epicder <epicder11@gmail.com>
2024-04-18 20:12:42 +0000

    commit

--- la_shellula.c
+++ la_shellula.c
@@ -1,4 +1,5 @@
 #include "simple_shell.h"
+#define ARGV_SIZE 32
 
 /**
  * main - main function
@@ -55,29 +56,40 @@ char *read_line(void)
  */
 void execute_command(char *cmd)
 {
-pid_t pid;
-int pidnumber;
- char *argv[] = {NULL};
-        pid = fork();
-        if (pid == -1)
-        {
-                perror("fork_error");
-                exit(EXIT_FAILURE);
-        }
-        if (pid == 0)
-        {
-          if (execve(cmd, argv, NULL) == -1)
-                {
-                        perror("execve_error");
-                        exit(EXIT_FAILURE);
-                }
-        }
-        else
-        {
-                if (waitpid(pid, &pidnumber, 0) == -1)
-                {
-                        perror("waitpid");
-                        exit(EXIT_FAILURE);
-                }
-        }
+	int i = 0;
+	pid_t pid;
+	char *token;
+	int pidnumber;
+	char *argv[ARGV_SIZE] = {0};
+	token = strtok(cmd, " \n\t");
+	argv[0] = token;
+	while (argv[i] != NULL)
+	{
+		i++;
+		token = strtok(NULL, " \n\t");
+		argv[i] = token;
+	}
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork_error");
+		exit(EXIT_FAILURE);
+	}
+
+	if (pid == 0)
+	{
+		if (execve(argv[0], argv, NULL) == -1)
+		{
+			perror("execve_error");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (waitpid(pid, &pidnumber, 0) == -1)
+		{
+			perror("waitpid");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
