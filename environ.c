5acbb1bc5622373d573401cbeb289275d49f32f2
Alison <8661@holbertonstudents.com>
2024-04-26 22:05:40 +0000

    hope valgrind works

--- environ.c
+++ environ.c
@@ -1,12 +1,22 @@
 #include "simple_shell.h"
 
+/**
+ * print_env - prints environment
+ */
+
 extern char **environ;
 void print_env(void)
 {
 	char **env_var = environ;
+	if (*env_var == NULL)
+	{
+		fprintf(stderr, "Error: environ is NULL\n");
+		return;
+	}
 	while (*env_var != NULL)
 	{
-		printf("%s\n", *env_var);
+		if (*env_var != NULL)
+			printf("%s\n", *env_var);
 		env_var++;
 	}
 }
